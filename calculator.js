f79b1be93252e548321e54ceb7b0311bc72a9aa3
Montinevra <23004878+montinevra@users.noreply.github.com>
2023-03-08 22:45:54 -0700

    generate tables from code

--- calculator.js
+++ calculator.js
@@ -7,6 +7,13 @@
 // 	a.value = parseInt(b.value);
 // }
 
+const snake_case = (string) => {
+	return string.replace(/\d+/g, ' ')
+		.split(/ |\B(?=[A-Z])/)
+		.map((word) => word.toLowerCase())
+		.join('_');
+}
+
 function update_point_total() {
 	var points_array = document.getElementsByClassName('points');  
 	var points = 0;
@@ -45,4 +52,27 @@ function update_skill_points(ranks, points, bonus) {
 		points.setAttribute('style', '');
 	}
 	update_point_total();
+}
+
+function on_load() {
+	for (var i = 0; i < talents.length; ++i) {
+		// TODO set base talents via race selection:
+		// TODO <td><output id="${snake_case(talents[i])}_base">5</output> + </td>\n\
+		document.getElementById("talent_list").innerHTML += `<tr>\n
+				<th>${talents[i]}</th> \n
+				<td><output id="${snake_case(talents[i])}_total">5</output> = </td>\n
+				<td><input type="number" min="4" max="9" value="5" id="${snake_case(talents[i])}_base" oninput="update_talent_points(${snake_case(talents[i])}_total, ${snake_case(talents[i])}_base, ${snake_case(talents[i])}_ranks, ${snake_case(talents[i])}_points)"/> + </td>\n
+				<td><input type="number" min="-3" max="5" value="0" id="${snake_case(talents[i])}_ranks" oninput="update_talent_points(${snake_case(talents[i])}_total, ${snake_case(talents[i])}_base, this, ${snake_case(talents[i])}_points)"/></td>\n
+				<td><output class="points" id="${snake_case(talents[i])}_points">0</output></td>\n
+				</tr>\n`;
+	}
+	for (var i = 0; i < skills.length; ++i) {
+		document.getElementById("skill_list").innerHTML += `<tr>\n
+				<th>${skills[i]}</th>\n
+				<td><input type="number" min="0" max="5" value="0" id="${snake_case(skills[i])}_ranks" oninput="update_skill_points(${snake_case(skills[i])}_ranks, ${snake_case(skills[i])}_points, ${snake_case(skills[i])}_bonus)"/></td>\n
+				<td><output class="points" id="${snake_case(skills[i])}_points">0</output></td>\n
+				<td><input type="number" min="0" value="0" id="${snake_case(skills[i])}_bonus" oninput="update_skill_points(${snake_case(skills[i])}_ranks, ${snake_case(skills[i])}_points, ${snake_case(skills[i])}_bonus)"/></td>\n
+				</tr>`
+	}
+	document.getElementById("char_calc").reset()
 }
