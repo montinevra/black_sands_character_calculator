5baac71ad7ffb81b07572179f67ac055f5992411
Montinevra <23004878+montinevra@users.noreply.github.com>
2023-03-29 20:10:41 -0700

    set base talents by selecting race

--- calculator.js
+++ calculator.js
@@ -43,6 +43,31 @@ function update_talent_points(total, base, float, ranks, points) {
 	update_point_total();
 }
 
+function update_talent(talent) {
+	let rank = parseInt(document.getElementById(`${talent}_ranks`).value);
+	let new_total = rank + parseInt(document.getElementById(`${talent}_base`).value) + parseInt(document.getElementById(`${talent}_float`).value);
+	let new_points = 0;
+
+	document.getElementById(`${talent}_total`).value = new_total;
+	if (rank <= 0) {
+		document.getElementById(`${talent}_points`).value = rank * 5;
+	} else {
+		while (rank > 0) {
+			new_points += new_total * rank;
+			new_total--;
+			rank--;
+		}
+		document.getElementById(`${talent}_points`).value = new_points;
+	}
+	update_point_total();
+}
+
+function update_race() {
+	for (const [key, value] of Object.entries(races[race.value].talents)) {
+		document.getElementById(`${key}_base`).value = value;
+		update_talent(key);
+	}
+}
 
 function update_skill_points(ranks, points, bonus) {
 	points.value = parseInt(ranks.value) + parseInt(ranks.value) ** 2 - parseInt(bonus.value);
@@ -57,12 +82,12 @@ function update_skill_points(ranks, points, bonus) {
 
 function on_load() {
 	for (talent of talents) {
-		// TODO set base talents via race selection:
-		// TODO <td><output id="${snake_case(talent)}_base">5</output> + </td>\n\
 		document.getElementById("talent_list").innerHTML += `<tr>\n
 				<th>${talent}</th> \n
 				<td><output id="${snake_case(talent)}_total">5</output> = </td>\n
-				<td><input type="number" min="4" max="9" value="5" id="${snake_case(talent)}_base" oninput="update_talent_points(${snake_case(talent)}_total, ${snake_case(talent)}_base, ${snake_case(talent)}_float, ${snake_case(talent)}_ranks, ${snake_case(talent)}_points)"/> + </td>\n
+			
+
+				<td><output id="${snake_case(talent)}_base">5</output> + </td>\n\
 				<td><input type="number" min="0" max="2" value="0" id="${snake_case(talent)}_float" oninput="update_talent_points(${snake_case(talent)}_total, ${snake_case(talent)}_base, ${snake_case(talent)}_float, ${snake_case(talent)}_ranks, ${snake_case(talent)}_points)"/> + </td>\n
 				<td><input type="number" min="-3" max="5" value="0" id="${snake_case(talent)}_ranks" oninput="update_talent_points(${snake_case(talent)}_total, ${snake_case(talent)}_base, ${snake_case(talent)}_float, this, ${snake_case(talent)}_points)"/></td>\n
 				<td><output class="points" id="${snake_case(talent)}_points">0</output></td>\n
@@ -77,4 +102,5 @@ function on_load() {
 				</tr>`
 	}
 	document.getElementById("char_calc").reset()
+	update_race();
 }
